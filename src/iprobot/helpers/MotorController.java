0409106e40d7634506252a6d35d7533ea65b0b97
yoann <yoann@192.168.0.153>
2018-01-25 00:27:36 -0500

    create Motor driver library and updated unit test accordingly

---
+++ src/iprobot/helpers/MotorController.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iprobot.helpers;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author yoann
+ */
+public class MotorController {
+
+    GpioController gpio;
+    GpioPinDigitalOutput In1pin, In2pin, STBYpin;
+    GpioPinPwmOutput PWMpin;
+    int PWMRange;
+
+    public GpioController getGpio() {
+        return gpio;
+    }
+
+    public void setGpio(GpioController gpio) {
+        this.gpio = gpio;
+    }
+
+    public GpioPinDigitalOutput getIn1pin() {
+        return In1pin;
+    }
+
+    public void setIn1pin(GpioPinDigitalOutput In1pin) {
+        this.In1pin = In1pin;
+    }
+
+    public GpioPinDigitalOutput getIn2pin() {
+        return In2pin;
+    }
+
+    public void setIn2pin(GpioPinDigitalOutput In2pin) {
+        this.In2pin = In2pin;
+    }
+
+    public GpioPinDigitalOutput getSTBYpin() {
+        return STBYpin;
+    }
+
+    public void setSTBYpin(GpioPinDigitalOutput STBYpin) {
+        this.STBYpin = STBYpin;
+    }
+
+    public GpioPinPwmOutput getPWMpin() {
+        return PWMpin;
+    }
+
+    public void setPWMpin(GpioPinPwmOutput PWMpin) {
+        this.PWMpin = PWMpin;
+    }
+
+    public int getPWMRange() {
+        return PWMRange;
+    }
+
+    public void setPWMRange(int PWMRange) {
+        this.PWMRange = PWMRange;
+    }
+
+    public MotorController(GpioController gpioController, Pin in1pin, Pin in2pin, Pin pWMpin, Pin sTBYpin) {
+        gpio = gpioController;
+        In1pin = gpio.provisionDigitalOutputPin(in1pin, PinState.LOW);
+        In2pin = gpio.provisionDigitalOutputPin(in2pin, PinState.LOW);
+        PWMpin = gpio.provisionPwmOutputPin(pWMpin);
+        STBYpin = gpio.provisionDigitalOutputPin(sTBYpin, PinState.LOW);
+        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
+        com.pi4j.wiringpi.Gpio.pwmSetRange(255);
+        com.pi4j.wiringpi.Gpio.pwmSetClock(500);
+        PWMRange = 255;
+
+    }
+
+    public MotorController(GpioController gpioController, Pin in1pin, Pin in2pin, Pin pWMpin, Pin sTBYpin, int pWMRange) {
+        gpio = gpioController;
+        In1pin = gpio.provisionDigitalOutputPin(in1pin, PinState.LOW);
+        In2pin = gpio.provisionDigitalOutputPin(in2pin, PinState.LOW);
+        PWMpin = gpio.provisionPwmOutputPin(pWMpin);
+        STBYpin = gpio.provisionDigitalOutputPin(sTBYpin, PinState.LOW);
+        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
+        com.pi4j.wiringpi.Gpio.pwmSetRange(pWMRange);
+        com.pi4j.wiringpi.Gpio.pwmSetClock(500);
+        PWMRange = pWMRange;
+
+    }
+
+    public void drive(int speed) {
+
+        if (Math.abs(speed) > PWMRange) {
+            speed = (int) (Math.signum(speed) * PWMRange);
+        }
+        STBYpin.setState(PinState.HIGH);
+        if (speed >= 0) {
+            forward(speed);
+        } else {
+            reverse(-speed);
+        }
+    }
+
+    public void drive(int speed, int durationMs) {
+        STBYpin.setState(PinState.HIGH);
+        if (speed >= 0) {
+            forward(speed);
+        } else {
+            reverse(-speed);
+        }
+        try {
+            Thread.sleep(durationMs);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(MotorController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void forward(int speed) {
+        In1pin.setState(PinState.HIGH);
+        In2pin.setState(PinState.LOW);
+        PWMpin.setPwm(speed);
+
+    }
+
+    private void reverse(int speed) {
+        In2pin.setState(PinState.HIGH);
+        In1pin.setState(PinState.LOW);
+        PWMpin.setPwm(speed);
+
+    }
+
+    void brake() {
+        In2pin.setState(PinState.HIGH);
+        In1pin.setState(PinState.HIGH);
+        PWMpin.setPwm(0);
+
+    }
+
+    public void changeDirection() {
+        In2pin.setState(!(In2pin.getState().isHigh()));
+        In1pin.setState(!(In1pin.getState().isHigh()));
+
+    }
+
+    public void standby() {
+        STBYpin.setState(PinState.LOW);
+    }
+
+    static public void forward(MotorController motor1, MotorController motor2, int speed) {
+        motor1.drive(speed);
+        motor2.drive(speed);
+    }
+
+    static public void forward(MotorController motor1, MotorController motor2) {
+        motor1.drive(motor1.PWMRange / 2);
+        motor2.drive(motor2.PWMRange / 2);
+    }
+
+    static public void back(MotorController motor1, MotorController motor2, int speed) {
+        int temp = Math.abs(speed);
+        motor1.drive(-temp);
+        motor2.drive(-temp);
+    }
+
+    static public void back(MotorController motor1, MotorController motor2) {
+        motor1.drive(-motor1.PWMRange / 2);
+        motor2.drive(-motor2.PWMRange / 2);
+    }
+
+    static public void left(MotorController left, MotorController right, int speed) {
+        int temp = Math.abs(speed) / 2;
+        left.drive(-temp);
+        right.drive(temp);
+
+    }
+
+    static public void right(MotorController left, MotorController right, int speed) {
+        int temp = Math.abs(speed) / 2;
+        left.drive(temp);
+        right.drive(-temp);
+
+    }
+
+    static public void brake(MotorController motor1, MotorController motor2) {
+        motor1.brake();
+        motor2.brake();
+    }
+
+}
--- test/MotorDriverUnitaryTest.java
+++ test/MotorDriverUnitaryTest.java
@@ -7,6 +7,7 @@ import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.util.CommandArgumentParser;
+import iprobot.helpers.MotorController;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,54 +26,76 @@ public class MotorDriverUnitaryTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // create GPIO controller instance
-        GpioController gpio = GpioFactory.getInstance();
-        Pin pin = CommandArgumentParser.getPin(
-                RaspiPin.class, // pin provider class to obtain pin instance from
-                RaspiPin.GPIO_01, // default pin if no pin argument found
-                args);             // argument array to search in
-
-        //setup Pwm pin
-        GpioPinPwmOutput pwm = gpio.provisionPwmOutputPin(pin);
-        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
-        com.pi4j.wiringpi.Gpio.pwmSetRange(1000);
-        com.pi4j.wiringpi.Gpio.pwmSetClock(500);
-
-        //setup standBy pin
-        GpioPinDigitalOutput standbyMotors = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, // PIN NUMBER
-                "stbyMotor", // PIN FRIENDLY NAME (optional)
-                PinState.HIGH);
-
-        //setup direction pin
-        GpioPinDigitalOutput dirMotorTest = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, // PIN NUMBER
-                "DirMotorTest", // PIN FRIENDLY NAME (optional)
-                PinState.HIGH);
-
+    GpioController gpio = GpioFactory.getInstance();
+        MotorController motor = new MotorController(gpio, RaspiPin.GPIO_00, RaspiPin.GPIO_02, RaspiPin.GPIO_01, RaspiPin.GPIO_03);
         int cpt = 0;
         int inc = 1;
         while (true) {
 
-            if (cpt >= 1000) {
+            if (cpt >= 255) {
                 inc = -1;
             }
-            if (cpt <= 0) {
+            if (cpt <= -255) {
                 inc = 1;
             }
             cpt += inc;
-            pwm.setPwm(cpt);
-            System.out.println("PWM rate is: " + pwm.getPwm());
-            if (cpt == 0) {
-                dirMotorTest.toggle();
-                System.out.println("direction changed");
-            }
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(MotorDriverUnitaryTest.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
+            motor.drive(cpt,5);
+            System.out.println("PWM rate is: " + cpt);
         }
-
+    
+    
+    
     }
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        // create GPIO controller instance
+//        GpioController gpio = GpioFactory.getInstance();
+//        Pin pin = CommandArgumentParser.getPin(RaspiPin.class, // pin provider class to obtain pin instance from
+//                RaspiPin.GPIO_01, "");             // argument array to search in
+//
+//        //setup Pwm pin
+//        GpioPinPwmOutput pwm = gpio.provisionPwmOutputPin(pin);
+//        com.pi4j.wiringpi.Gpio.pwmSetMode(com.pi4j.wiringpi.Gpio.PWM_MODE_MS);
+//        com.pi4j.wiringpi.Gpio.pwmSetRange(1000);
+//        com.pi4j.wiringpi.Gpio.pwmSetClock(500);
+//
+//        //setup standBy pin
+//        GpioPinDigitalOutput standbyMotors = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, // PIN NUMBER
+//                "stbyMotor", // PIN FRIENDLY NAME (optional)
+//                PinState.HIGH);
+//
+//        //setup direction pin
+//        GpioPinDigitalOutput dirMotorTest = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, // PIN NUMBER
+//                "DirMotorTest", // PIN FRIENDLY NAME (optional)
+//                PinState.HIGH);
+//
+//        int cpt = 0;
+//        int inc = 1;
+//        while (true) {
+//
+//            if (cpt >= 1000) {
+//                inc = -1;
+//            }
+//            if (cpt <= 0) {
+//                inc = 1;
+//            }
+//            cpt += inc;
+//            pwm.setPwm(cpt);
+//            System.out.println("PWM rate is: " + pwm.getPwm());
+//            if (cpt == 0) {
+//                dirMotorTest.toggle();
+//                System.out.println("direction changed");
+//            }
+//            try {
+//                Thread.sleep(5);
+//            } catch (InterruptedException ex) {
+//                Logger.getLogger(MotorDriverUnitaryTest.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//
+//        }
+//
+//    }
 
 }
