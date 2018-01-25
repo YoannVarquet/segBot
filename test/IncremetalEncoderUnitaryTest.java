71e4bf17eccb2180597c8a1be5945d34ecec7598
yoann <yoann@192.168.0.153>
2018-01-25 11:59:58 -0500

    create test encoder to be tested on Pi before creating helper

--- test/IncremetalEncoderUnitaryTest.java
+++ test/IncremetalEncoderUnitaryTest.java
@@ -1,9 +1,17 @@
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author yoann
@@ -13,8 +21,33 @@ public class IncremetalEncoderUnitaryTest {
     /**
      * @param args the command line arguments
      */
+    static GpioPinDigitalInput wheelCoder;
+    static boolean wheelCoderUpdated = false;
+    static long estimatedTime = 0;
+    static final double ANGLE_INCREMENT = Math.PI / 10;
+    static int wheelCpt = 0;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
+        wheelCoder = gpio.provisionDigitalInputPin(RaspiPin.GPIO_05, PinPullResistance.PULL_DOWN);        // set shutdown state for this input pin
+        wheelCoder.setShutdownOptions(true);
+        System.out.println("WheelEncoder setup");
+
+        // create and register gpio pin listener
+        wheelCoder.addListener((GpioPinListenerDigital) (GpioPinDigitalStateChangeEvent event) -> {
+            estimatedTime = System.currentTimeMillis() - estimatedTime;
+//            if (estimatedTime > 1000) {
+//                wheelCoderUpdated = true;
+//            }
+            wheelCpt++;
+        });
+
+        while (true) {
+            System.out.println(wheelCpt);
+        }
     }
-    
+
 }
