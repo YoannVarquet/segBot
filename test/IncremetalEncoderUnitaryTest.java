eeb4626cd76d2a065b02174a4d9d2b55be384671
yoann <yoann@192.168.0.153>
2018-01-26 03:21:53 +0100

    encoder tested and working. TODO pid and librarize(?)

--- test/IncremetalEncoderUnitaryTest.java
+++ test/IncremetalEncoderUnitaryTest.java
@@ -26,12 +26,14 @@ public class IncremetalEncoderUnitaryTest {
     static GpioPinDigitalInput wheelCoder;
     static boolean wheelCoderUpdated = false;
     static long estimatedTime = 0;
+    static long previousTime = 0;
     static final double ANGLE_INCREMENT = Math.PI / 10;
     static int wheelCpt = 0;
 
     public static void main(String[] args) {
         // create gpio controller
         final GpioController gpio = GpioFactory.getInstance();
+        MotorController motor = new MotorController(gpio, RaspiPin.GPIO_04, RaspiPin.GPIO_02, RaspiPin.GPIO_01, RaspiPin.GPIO_03);
 
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
         wheelCoder = gpio.provisionDigitalInputPin(RaspiPin.GPIO_25, PinPullResistance.PULL_DOWN);        // set shutdown state for this input pin
@@ -41,15 +43,17 @@ public class IncremetalEncoderUnitaryTest {
         // create and register gpio pin listener
         estimatedTime = System.currentTimeMillis();
         wheelCoder.addListener((GpioPinListenerDigital) (GpioPinDigitalStateChangeEvent event) -> {
-            estimatedTime = System.currentTimeMillis() - estimatedTime;
-            if (estimatedTime > 50) {
-                wheelCoderUpdated = true;
-            }
             if (event.getEdge() == PinEdge.RISING) {
                 wheelCpt++;
             }
+//            motor.brake();
+            estimatedTime = System.currentTimeMillis() - previousTime;
+            if (estimatedTime > 200 && !wheelCoderUpdated) {
+                wheelCoderUpdated = true;
+            }
+
+//            motor.drive(255);
         });
-        MotorController motor = new MotorController(gpio, RaspiPin.GPIO_04, RaspiPin.GPIO_02, RaspiPin.GPIO_01, RaspiPin.GPIO_03);
 
         //PID 
         double error_prior = 0;
@@ -57,12 +61,19 @@ public class IncremetalEncoderUnitaryTest {
         double kP = 10;
         double kI = 0.01;
         double kD = 1;
-long speedRpm, pulsePerSecond;
+        motor.brake();
+        double speedRpm, pulsePerSecond;
+        motor.drive(255);
         while (true) {
             if (wheelCoderUpdated) {
-                 pulsePerSecond = wheelCpt*1000/estimatedTime;
-                 speedRpm = pulsePerSecond *60/20;
-                 System.out.println("speedRpm= "+ speedRpm);
+                wheelCoderUpdated = false;
+                //    motor.brake();
+                pulsePerSecond = (wheelCpt * 1000.0) / (double) estimatedTime;
+                speedRpm = (pulsePerSecond * 60.0) / 20.0;
+                System.out.println("speedRpm= " + speedRpm + "\twheelCpt= " + wheelCpt + "\testimatedTime= " + estimatedTime);
+                // motor.drive(255);
+                wheelCpt = 0;
+                previousTime = System.currentTimeMillis();
 //            motor.drive(100, 1070);
 //            motor.brake();
 //            System.out.println(wheelCpt);
@@ -76,8 +87,6 @@ long speedRpm, pulsePerSecond;
 //                    error_prior = error;
 ////                sleep(iteration_time);
 //                }
-
-                estimatedTime = System.currentTimeMillis();
             }
         }
 
